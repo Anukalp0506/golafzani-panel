3491e9984d3dace5da562a14e1e9f01204188521
Anukalp0506 <160638306+Anukalp0506@users.noreply.github.com>
2024-09-01 12:08:55 +0530

    Update golafzani_worker.js

--- golafzani_worker.js
+++ golafzani_worker.js
@@ -9,7 +9,7 @@
 
 import { connect } from 'cloudflare:sockets';
 // یویوآیدی را تنظیم کنید
-let userID = 'f1a234f6-c946-4024-ba79-882f58c22727';
+let userID = '67e930c5-db0b-4a65-ab3a-eebd9990036e';
 
 //This is now a default ProxyIP system - you can change it but be careful!
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
@@ -23,7 +23,7 @@ let พร็อกซีไอพี = พร็อกซีไอพีs[Math.
 // ipv6 พร็อกซีไอพี مثال حذف نظر برای استفاده
 // اجازه دهید พร็อกซีไอพี = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://dns.google/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 //Better to use IpTamiz from this bot Claxpoint recommend
 // The Best Full Library of IpTamiz Updating everytime and free on telegram bot :
